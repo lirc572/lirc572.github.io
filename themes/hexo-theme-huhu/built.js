6c1fccc44399e1f40f36542467577889938035a3
lirc572 <lirc572@gmail.com>
2019-12-15 17:26:17 +0800

    added theme huhu

---
+++ themes/hexo-theme-huhu/built.js
@@ -0,0 +1,23 @@
+({
+  baseUrl: './source/scripts',
+  optimize: 'none',
+  name: './app',
+  out: './source/scripts/app-built.js',
+  removeCombined: true,
+  paths: {
+    // cdn
+    jquery: 'empty:',
+    av: 'empty:',
+    pjax: 'empty:',
+    confirm: 'empty:',
+    fancybox: 'empty:',
+    valine: 'empty:',
+    chart: 'empty:',
+
+    // need built
+    util: 'util',
+    share: 'share',
+    search: 'search',
+    registerSW: 'registerSW'
+  }
+})
